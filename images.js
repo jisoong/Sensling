15d84bf70346fde9c5fe726d076dea6af13c417d
Jisu Kang <kangjs0237@gmail.com>
2022-06-09 15:29:49 +0900

    update main page

--- images.js
+++ images.js
@@ -1,10 +1,10 @@
 function showImage(){
   let imgName = ['sky', 'forest', 'ocean', 'flower'];
-  let randName = imgName[Math.floor(Math.random() * 3)]
+  let randName = imgName[Math.floor(Math.random() * 4)]
 
   let randNumArray = []
   for (i=0; i<4; i++) {
-    randomNum = Math.floor(Math.random() * 15)
+    randomNum = Math.floor(Math.random() * 16)
     if (randNumArray.indexOf(randomNum) === -1) {
       randNumArray.push(randomNum)
     } else {
@@ -17,7 +17,7 @@ function showImage(){
   let randImg3 = randName + randNumArray[2]
   let randImg4 = randName + randNumArray[3]
 
-  console.log(randName + randNumArray[0])
+  console.log(randImg)
   
   var objImg = document.getElementById("randImg1");
   objImg.src = 'images/'+ randName + '/' + randImg + '.jpg';

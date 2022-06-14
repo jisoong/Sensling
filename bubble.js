f674de91bdec51cff7b54a235f8d069a403eba25
Jisu Kang <kangjs0237@gmail.com>
2022-06-14 15:45:46 +0900

    update website

--- bubble.js
+++ bubble.js
@@ -1,29 +1,19 @@
 function showBubble() {
   const bubbElementList = document.querySelectorAll('.bubble');
-  let bubbTriggerMargin = 850
+  let bubbTriggerMargin = 500
   const bubbFunc = function() {
     let bubb = document.getElementById("bubble");
     function bubbleHide() {
       setTimeout(function() {
         bubb.style.opacity  = 0
-      }, 3000);
-    };
+      }, 6000);
+    }
     function bubbleShow() {
       bubb.style.opacity = 1
-    };
-
-    if (bubb.style.opacity == 1){
-      let start = 0
     }
-    
-
 
     for (const element of bubbElementList) {
-      if (start = 0) {
-        element.classList.add('show'); 
-        start += 1
-      }
-      
+
       if (!element.classList.contains('show')) {
          if (window.innerHeight > element.getBoundingClientRect().top + bubbTriggerMargin) {
             bubbleHide();
@@ -39,10 +29,11 @@ function showBubble() {
           element.classList.add('hide');  
        }
       }
-    
-   }
+    }
+
+  }
+  
   window.addEventListener('load', bubbFunc);
   window.addEventListener('scroll', bubbFunc);
-  }
 }
 
---
+++ m_player.js
@@ -0,0 +1,3 @@
+setTimeout(function() {
+  document.getElementById("player").style.opacity  = 0
+}, 7000);

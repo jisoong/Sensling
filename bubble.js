db16a7e8bf65bbd2fa9471fe76c254dac0d320d5
Jisu Kang <kangjs0237@gmail.com>
2022-06-14 14:59:44 +0900

    update website

--- bubble.js
+++ bubble.js
@@ -1,48 +1,48 @@
-
-// function bubbleHide() {
-//   let bubb = document.getElementById("bubble");
-//   setTimeout(function() {
-//     bubb.style.opacity  = 0
-//   }, 3000);
-// }
-let bubbTriggerMargin = 850; 
-function showBubble(bubbTriggerMargin) {
+function showBubble() {
   const bubbElementList = document.querySelectorAll('.bubble');
-  
+  let bubbTriggerMargin = 850
   const bubbFunc = function() {
     let bubb = document.getElementById("bubble");
     function bubbleHide() {
-      
       setTimeout(function() {
         bubb.style.opacity  = 0
-      }, 4000);
-    }
+      }, 3000);
+    };
     function bubbleShow() {
       bubb.style.opacity = 1
+    };
+
+    if (bubb.style.opacity == 1){
+      let start = 0
     }
+    
+
 
     for (const element of bubbElementList) {
-
+      if (start = 0) {
+        element.classList.add('show'); 
+        start += 1
+      }
+      
       if (!element.classList.contains('show')) {
-        if (window.innerHeight > element.getBoundingClientRect().top + bubbTriggerMargin) {
-          element.classList.remove('hide');
-          bubbleHide();
-          element.classList.add('show');
-        }
+         if (window.innerHeight > element.getBoundingClientRect().top + bubbTriggerMargin) {
+            bubbleHide();
+            element.classList.remove('hide');
+            element.classList.add('show');  
+         }
       }
   
       if (element.classList.contains('show')) {
-        // if (window.innerHeight < element.getBoundingClientRect().top + bubbTriggerMargin) {
-          element.classList.remove('show');
+        if (window.innerHeight < element.getBoundingClientRect().top + bubbTriggerMargin) {
           bubbleShow();
-          element.classList.add('hide');
-        // }
+          element.classList.remove('show');
+          element.classList.add('hide');  
+       }
       }
-    }
-  
-  }
-  
+    
+   }
   window.addEventListener('load', bubbFunc);
   window.addEventListener('scroll', bubbFunc);
+  }
 }
 

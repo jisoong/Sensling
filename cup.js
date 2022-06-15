4c9b717154759b1458510b30e304fbc0c2db9c55
Jisu Kang <kangjs0237@gmail.com>
2022-06-15 17:13:22 +0900

    update website(tea.html)

--- cup.js
+++ cup.js
@@ -2,24 +2,23 @@
 $(document).ready(function(){       
 	var scroll_pos = 0;
 	var last_scrollTop = 0;
-	var num = 1
-	const pencil = document.getElementById("pencil");
+	var num = 0
+	const cup = document.getElementById("cup");
 	$(window).scroll(function () {
 			var tmp = $(this).scrollTop();
 			scroll_pos = $(this).scrollTop();
-			if (tmp > last_scrollTop && num <83 && scroll_pos < $("#pencil").height()) {
-				num = num + 1
-				pencil.src = 'images/pencil/pencil' + '('+ + num + ')' + '.png';
-        // console.log(pencil.src)
+			if (tmp > last_scrollTop && num <270) {
+				num = num + 2
+				cup.src = 'images/cup/cup' + '_'+ + num + '.png';
+        console.log(cup.src)
 			}
-			else if (tmp <= last_scrollTop && num <=83 && num>1) {
+			else if (tmp <= last_scrollTop && num <=270 && num>1) {
         // console.log()
 				num = num - 1
-				pencil.src = 'images/pencil/pencil' + '('+ + num + ')' + '.png';
+				cup.src = 'images/cup/cup' + '_'+ + num + '.png';
 				// console.log(pencil.src)
 			}
 			last_scrollTop = tmp;
 	});
 
 	});
-

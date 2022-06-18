21852509e6469a8bf0aba00c64ce2ec97dd7e665
Jisu Kang <kangjs0237@gmail.com>
2022-06-19 03:39:58 +0900

    delete file

--- date.js
+++
@@ -1,9 +0,0 @@
-function date() {
-    let today = new Date();
-
-    let year = today.getFullYear(); // 년도
-    let month = today.getMonth() + 1;  // 월
-    let date = today.getDate();  // 날짜
-
-    document.getElementById("diary_date").innerHTML = year + ' / ' + month + ' / ' + date;
-}

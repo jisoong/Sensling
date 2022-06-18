3b46347bcd4029f6d00c5244792158aef568aa9e
Jisu Kang <kangjs0237@gmail.com>
2022-06-19 04:51:11 +0900

    update website

--- textBubble_diary.js
+++ textBubble_diary.js
@@ -1,51 +1,55 @@
-var target = document.querySelectorAll(".bubb")
+var targetB = document.querySelectorAll(".bubb")
 
-function bubbleHide(i) {
+function bubbleHide_D(i) {
   setTimeout(function() {
-    target[i].style.opacity  = 0
+    targetB[i].style.opacity  = 0
   }, 6000);
 }
 
-function bubbleShow(i) {
-  target[i].style.opacity  = 1
+function bubbleShow_D(i) {
+  targetB[i].style.opacity  = 1
 }
 
 $(window).scroll(function () {
   var tmp = $(this).scrollTop();
   scroll_pos = $(this).scrollTop();
-  // console.log(tmp)
+  console.log(tmp)
   // console.log(window.innerHeight*0.75) 
-  if (tmp > window.innerHeight*0.5){
-    bubbleHide(1)
-    target[1].classList.remove('show')
-    target[1].classList.add('hide')
-    bubbleHide(0)
-    target[0].classList.remove('show')
-    target[0].classList.add('hide')
-    bubbleHide(2)
-    target[2].classList.remove('show')
-    target[2].classList.add('hide')
+  if (tmp > window.innerHeight*1.1){
+
+    bubbleHide_D(0)
+    targetB[0].classList.remove('show')
+    targetB[0].classList.add('hide')
+
     // console.log("hide")
   }
-  if (target[1].classList.contains('hide')){
-    if (tmp < window.innerHeight*0.6){
-      bubbleShow(1)
-      target[1].classList.remove('hide');
-      target[1].classList.add('show');  
+  if (tmp > window.innerHeight*2.5) {
+    bubbleHide_D(1)
+    targetB[1].classList.remove('show')
+    targetB[1].classList.add('hide')
+    bubbleHide_D(2)
+    targetB[2].classList.remove('show')
+    targetB[2].classList.add('hide')
+  }
+  if (targetB[1].classList.contains('hide')){
+    if (tmp < window.innerHeight*2.0){
+      bubbleShow_D(1)
+      targetB[1].classList.remove('hide');
+      targetB[1].classList.add('show');  
     }
   }
-  if (target[0].classList.contains('hide')){
-    if (tmp < window.innerHeight*0.6){
-      bubbleShow(0)
-      target[0].classList.remove('hide');
-      target[0].classList.add('show');  
+  if (targetB[0].classList.contains('hide')){
+    if (tmp < window.innerHeight*1.9){
+      bubbleShow_D(0)
+      targetB[0].classList.remove('hide');
+      targetB[0].classList.add('show');  
     }
   }
-  if (target[0].classList.contains('hide')){
-    if (tmp < window.innerHeight*0.6) {
-      bubbleShow(2)
-      target[2].classList.remove('hide');
-      target[2].classList.add('show')
+  if (targetB[0].classList.contains('hide')){
+    if (tmp < window.innerHeight*2.0) {
+      bubbleShow_D(2)
+      targetB[2].classList.remove('hide');
+      targetB[2].classList.add('show')
     }
   }
 

52030f4403007b37f46785b01f7e446bdf2fd7f2
kchcau-21 <cksgud0630@naver.com>
2022-06-19 02:46:12 +0900

    change

--- imageScroll.js
+++ imageScroll.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
 			scroll_pos = $(this).scrollTop();
       console.log(scroll_pos)
       if(scroll_pos >= $("#diaryImg").height()*0.4) {
-        $("#diaryImg").fadeOut(2000);
+        $("#diaryImg").fadeOut(4000);
       } 
       else {
         $("#diaryImg").fadeIn(2000);
